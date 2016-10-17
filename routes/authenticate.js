fde6099d621eab53d3694b852809d784422b6267
magarcia <miguel.garcia.inge@gmail.com>
2016-10-17 16:59:19 -0500

    dasd

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -1,3 +1,44 @@
 /**
  * Created by chito on 16/10/2016.
  */
+var express = require('express');
+var router = express.Router();
+
+module.exports = function(passport){
+    //sends successful login state back to angular
+    router.get('/success',function (req,res) {
+        res.send({
+            state: 'success',
+            user: ((req.user)?(req.user):(null))
+        })
+    });
+
+    //sends failure login  state back to angular
+    router.get('/failure',function (req, res) {
+        res.send({
+            state: 'failure',
+            user: null,
+            message: "Usuario o password invalido."
+        });
+    });
+
+    //log in
+    router.post('/login',passport.authenticate('login',{
+        successRedirect: '/auth/success',
+        failureRedirect: '/auth/failure'
+    }));
+
+    //sign up
+    router.post('/signup',passport.authenticate('signup',{
+        successRedirect: '/auth/success',
+        failureRedirect: '/auth/failure'
+    }));
+
+    //log out
+    router.get('/signout',function(req,res){
+        req.logout();
+        res.redirect('/');
+    });
+
+    return router;
+}
